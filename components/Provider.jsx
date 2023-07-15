0b88db3de96e81f7e93b3da54b720c0fc25218d2
Joseph Yusuff <joseph.yusuff12@gmail.com>
2023-07-15 22:06:54 +0100

    Session Provider Higher order component

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+"use client";
 
-const Provider = () => {
-	return <div>Provider</div>;
+import { SessionProvider } from "next-auth/react";
+
+// We will use the session provider to wrap the code since it is a higher order component - meaning we are going to wrap other components with it
+
+const Provider = ({ children, session }) => {
+	return <SessionProvider session={session}>{children}</SessionProvider>;
 };
 
 export default Provider;
