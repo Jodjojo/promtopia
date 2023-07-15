553b330aa85069716c801f3474e99521ad8749ad
Joseph Yusuff <joseph.yusuff12@gmail.com>
2023-07-15 22:26:51 +0100

    Google client id and secret setting

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,12 +4,18 @@ import GoogleProvider from "next-auth/providers/google";
 const handler = NextAuth({
 	providers: [
 		GoogleProvider({
-			clientId: "",
-			clientSecret: "",
+			clientId: process.env.GOOGLE_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 		}),
 	],
 	async session({ session }) {},
-	async signIn({ profile }) {},
+	async signIn({ profile }) {
+		try {
+			//every next js route is known as a serverless route
+		} catch (error) {}
+	},
 });
 
 export { handler as GET, handler as POST };
+
+// we create the client id and secret from 'console.cloud.google.com'
