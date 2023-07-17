e1ea784279c4560238d63107d81148a0da852547
Joseph Yusuff <joseph.yusuff12@gmail.com>
2023-07-17 07:10:58 +0100

    Finalizing the Reading of our Prompts-Prompt card

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -6,7 +6,15 @@ import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
-	console.log(post.creator);
+	const [copied, setCopied] = useState("");
+
+	// copy to clipboard function
+	const handleCopy = () => {
+		setCopied(post.prompt);
+		navigator.clipboard.writeText(post.prompt);
+		setTimeout(() => setCopied(""), 3000);
+	};
+
 	return (
 		<div className='prompt_card'>
 			<div className='flex justify-between items-start gap-5'>
@@ -27,10 +35,26 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 						</p>
 					</div>
 				</div>
-				<div className='copy_btn' onClick={() => {}}>
-					{/* <Image src={}/> */}
+				<div className='copy_btn' onClick={handleCopy}>
+					{/* copied or cancel btn image on prompt cards */}
+					<Image
+						src={
+							copied === post.prompt
+								? "/assets/icons/tick.svg"
+								: "/assets/icons/copy.svg"
+						}
+						width={12}
+						height={12}
+					/>
 				</div>
 			</div>
+			<p className='my-4 font-satoshi text-sm text-gray-700'>{post.prompt}</p>
+			<p
+				className='font-inter text-sm blue_gradient cursor-pointer'
+				onClick={() => handleTagClick && handleTagClick(post.tag)}
+			>
+				{post.tag}
+			</p>
 		</div>
 	);
 };
